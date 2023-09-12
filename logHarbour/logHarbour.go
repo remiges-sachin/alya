2d55b0e4d603dd2776bee23e828aa92859fe0a25
Bhavya Gandhi <bhavya.gandhi@merce.co>
2023-09-12 13:08:02 +0530

    #16501 : removed custom logHarbour and made sLogHarbour as logHarbour

--- logHarbour/logHarbour.go
+++ logHarbour/logHarbour.go
@@ -1,28 +1,49 @@
-// logHarbour is logging framework from remiges.tech
-package logHarbour
+package sLogHarbour
 
 import (
-	"encoding/json"
+	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"os"
 	"runtime"
-	"runtime/debug"
 	"strconv"
-	"sync/atomic"
 	"time"
 )
 
-// log level to print. Will be used while printing of log messages to a defined output
-var globalLogLevel int64
+// Exported constants from a custom logging package.
+const (
+	LevelDebug2   = slog.Level(-8)
+	LevelDebug1   = slog.Level(-7)
+	LevelDebug    = slog.LevelDebug
+	LevelInfo     = slog.LevelInfo
+	LevelWarning  = slog.LevelWarn
+	LevelError    = slog.LevelError
+	LevelCritical = slog.Level(12)
+	LevelSec      = slog.Level(16)
+)
 
-// func sets global log level to level passed as argument
-func SetGlobalLogLevel(ll LogLevel) {
-	atomic.StoreInt64(&globalLogLevel, int64(ll))
+var LogHandle *slog.Logger
+
+type LogHandles struct {
+	ActivityLogger   *slog.Logger
+	DataChangeLogger *slog.Logger
+	DebugLogger      *slog.Logger
 }
 
-// go runtime version
-var goRuntime string
+const (
+	ACTIVITY_LOGGER   = "A"
+	DEBUG_LOGGER      = "D"
+	DATACHANGE_LOGGER = "C"
+	PRI_DEBUG2        = "DEBUG2"
+	PRI_DEBUG1        = "DEBUG1"
+	PRI_DEBUG         = "DEBUG"
+	PRI_INFO          = "INFO"
+	PRI_WARN          = "WARN"
+	PRI_ERROR         = "ERROR"
+	PRI_CRITICAL      = "CRIT"
+	PRI_SECURITY      = "SEC"
+)
 
 type appIdentifier struct {
 	App    string `json:"app"`
@@ -36,18 +57,98 @@ var identity appIdentifier
 var isInitalized bool
 
 // initializes logHarbour with app, module and system names
-func LogInit(appName, moduleName, systemName string) {
+func LogInit(appName, moduleName, systemName string) LogHandles {
 	//will allow initialization only once
 	if !isInitalized {
 		identity = appIdentifier{appName, moduleName, systemName}
 		isInitalized = true
 	}
+	return getLogger()
 }
 
-func init() {
-	SetGlobalLogLevel(Inf)
-	buildInfo, _ := debug.ReadBuildInfo()
-	goRuntime = buildInfo.GoVersion
+func manageAttributes(a slog.Attr) slog.Attr {
+	if a.Key == slog.TimeKey {
+		return slog.Attr{}
+	}
+	// Customize the name of the level key and the output string, including
+	// custom level values.
+	if a.Key == slog.LevelKey {
+		// Rename the level key from "level" to "sev".
+		a.Key = "pri"
+
+		// Handle custom level values.
+		level := a.Value.Any().(slog.Level)
+		// This could also look up the name from a map or other structure, but
+		// this demonstrates using a switch statement to rename levels. For
+		// maximum performance, the string values should be constants, but this
+		// example uses the raw strings for readability.
+		a.Value = getLogLevelString(level)
+	}
+	return a
+}
+
+func getLogLevelString(level slog.Level) (levelString slog.Value) {
+	switch {
+	case level <= LevelDebug2:
+		levelString = slog.StringValue(PRI_DEBUG2)
+	case level <= LevelDebug1:
+		levelString = slog.StringValue(PRI_DEBUG1)
+	case level <= LevelDebug:
+		levelString = slog.StringValue(PRI_DEBUG)
+	case level <= slog.LevelInfo:
+		levelString = slog.StringValue(PRI_INFO)
+	case level <= LevelWarning:
+		levelString = slog.StringValue(PRI_WARN)
+	case level <= LevelError:
+		levelString = slog.StringValue(PRI_ERROR)
+	case level <= LevelCritical:
+		levelString = slog.StringValue(PRI_CRITICAL)
+	default:
+		levelString = slog.StringValue(PRI_SECURITY)
+	}
+	return
+}
+
+func getLogger() LogHandles {
+	return LogHandles{slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: programLevel, ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+		return manageAttributes(a)
+	}})).With("handle", ACTIVITY_LOGGER),
+		slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: programLevel, ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			return manageAttributes(a)
+		}})).With("handle", DATACHANGE_LOGGER),
+		slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: programLevel, ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			return manageAttributes(a)
+		}})).With("handle", DEBUG_LOGGER).With("pid", os.Getpid()).With("source", getCaller(1)).With("callTrace", getCallTrace(1))}
+}
+
+var programLevel = new(slog.LevelVar) // Info by default
+
+// to change global zerolog logging level.
+func ChangeGlobalLogLevel(level slog.Level) {
+	programLevel.Set(level)
+	fmt.Printf("[%s]:GLOBAL log level set to: [%s]\n", time.Now().UTC(), getLogLevelString(level))
+}
+
+// struct for managing data change objects
+type DataChg struct {
+	Field  string `json:"field"`
+	OldVal string `json:"oldVal"`
+	NewVal string `json:"newVal"`
+}
+
+// func returns data change object
+func GetDataChg(field, oldVal, newVal string) DataChg {
+	return DataChg{field, oldVal, newVal}
+}
+
+// LogValue implements slog.LogValuer.
+// It returns a group containing the fields of
+// the Name, so that they appear together in the log output.
+func (dc DataChg) LogValue() slog.Value {
+	return slog.GroupValue(
+		slog.String("field", dc.Field),
+		slog.String("oldVal", dc.OldVal),
+		slog.String("newVal", dc.NewVal))
 }
 
 func getFrame(skipFrames int) runtime.Frame {
@@ -74,14 +175,13 @@ func getFrame(skipFrames int) runtime.Frame {
 }
 
 // getCallTrace returns the caller of the function that called it :)
-func getCallTrace() string {
+func getCallTrace(i int) string {
 	// Skip GetCallerFunctionName and the function to get the caller of
-	return getFrame(2).Function
+	return getFrame(2 + i).Function
 }
 
-// getCaller() returns the file and line no from where the func is called
-func getCaller() string {
-	_, file, line, _ := runtime.Caller(2)
+func getCaller(i int) string {
+	_, file, line, _ := runtime.Caller(2 + i)
 	short := file
 	for i := len(file) - 1; i > 0; i-- {
 		if file[i] == '/' {
@@ -93,126 +193,19 @@ func getCaller() string {
 	return file + ":" + strconv.Itoa(line)
 }
 
-// struct to write debug info to log messages
-type debugInfo struct {
-	Caller    string `json:"source,omitempty"`
-	CallTrace string `json:"callTrace,omitempty"`
-	Pid       int    `json:"pid,omitempty"`
-	GoVersion string `json:"goVersion,omitempty"`
-}
-
-// log level
-type LogLevel int64
-
-// log level constants
-const (
-	Inf LogLevel = 1
-	Err LogLevel = 2
-	Dbg LogLevel = 0
-	Trc LogLevel = -1
-)
-
-// func checks log level and returns a bool confirming whether the statement should actually be logged or not.
-func shouldPrint(logLevel LogLevel) bool {
-	if isInitalized {
-		return int64(logLevel) >= globalLogLevel
-	}
-	return false
-}
-
-// func returns a string based on log level
-func getLogString(ll LogLevel) string {
-	switch ll {
-	case Inf:
-		return "Info"
-	case Err:
-		return "Error"
-	case Dbg:
-		return "Debug"
-	case Trc:
-		return "Trace"
-	default:
-		return "Info"
-	}
-}
-
-// struct for logging messages
-type logMsg struct {
-	LogLevelInf string `json:"pri"` //could be "level" as per convention
-	appIdentifier
-	debugInfo
-	SpanId        string `json:"spanId"`
-	CorrelationId string `json:"correlationId"`
-	When          string `json:"when"`
-	Who           string `json:"who"`
-	RemoteIp      string `json:"remoteIp"`
-	Op            string `json:"op"`
-	What          string `json:"what"`
-	Status        int    `json:"status"`
-	Msg           string `json:"msg"`
-	Params        any    `json:"params,omitempty"`
-}
-
-// struct for managing data change objects
-type dataChg struct {
-	Field  string `json:"field"`
-	OldVal string `json:"oldVal"`
-	NewVal string `json:"newVal"`
-}
-
-// func returns data change object
-func GetDataChg(field, oldVal, newVal string) dataChg {
-	return dataChg{field, oldVal, newVal}
-}
-
-// func returns a key:value map for printing a json
-func GetKV(key string, val string) map[string]string {
-	a := map[string]string{key: val}
-	return a
-}
-
-// creates an empty interface if nothing is passed to the variadic function. This is imp so that the json parser skips the "params" tag while creating the message
-func checkAny(customMsgs ...any) any {
+func checkCustomMsg(customMsgs ...any) slog.Attr {
 	if len(customMsgs) > 0 {
-		return customMsgs
+		return slog.Any("params", customMsgs)
 	} else {
-		var emptyInterface interface{}
-		return emptyInterface
+		return slog.Attr{}
 	}
 }
 
-// writes Log.
-// TODO : check for possible sync issues
-func LogWrite(ll LogLevel, spanId, correlationId, when, who, remoteIp, op, what string, status int, msg string, customMsgs ...any) {
+// func writes log to specified source using slog
+func LogWrite(lgger *slog.Logger, ll slog.Level, spanId, correlationId, when, who, remoteIp, op, what string, status int, msg string, customMsgs ...any) {
 	if !isInitalized {
-		log.Fatalf("logHarbour not initialized. source[%s]. caller[%s]\n", getCallTrace(), getCaller())
+		log.Fatalf("logHarbour not initialized. source[%s]. caller[%s]\n", getCallTrace(0), getCaller(0))
 	}
-	if shouldPrint(ll) {
-		//if no time is passed, it'll print time of printing the log
-		if when == "" {
-			when = time.Now().Format("2006-01-02T15:04:05Z")
-		}
-		var lm logMsg
-		switch ll {
-		case Inf, Err:
-			lm = logMsg{getLogString(ll), identity, debugInfo{}, spanId, correlationId, when, who, remoteIp, op, what, status, msg, checkAny(customMsgs...)}
-		case Dbg, Trc:
-			lm = logMsg{getLogString(ll), identity, debugInfo{getCaller(), getCallTrace(), os.Getpid(), goRuntime}, spanId, correlationId, when, who, remoteIp, op, what, status, msg, checkAny(customMsgs...)}
-		}
-		sendLog(lm)
-	}
+	ctx := context.Background()
+	lgger.Log(ctx, ll, msg, "spanId", spanId, "correlationId", correlationId, "when", when, "who", who, "remoteIp", remoteIp, "op", op, "what", what, "status", status, checkCustomMsg(customMsgs...))
 }
-
-// this func decides where to send logs
-// TODO: create io function to write to output(file/port etc)
-// TODO: check for possible sync issues
-func sendLog(ls logMsg) {
-	json_data, err := json.Marshal(ls)
-	if err != nil {
-		log.Fatalf("Error while encoding to json msg[%v]: \nError: %v \n", ls, err)
-	}
-	fmt.Println(string(json_data))
-}
-
-//slog : structured log
-// loggger handler
--- sampleMain.go
+++ sampleMain.go
@@ -2,50 +2,44 @@ package main
 
 import (
 	"fmt"
-	"go-framework/logHarbour"
-	"go-framework/sLogHarbour"
+	logHarbour "go-framework/logHarbour"
 	"time"
 )
 
 var timeFormat string = "2006-01-02T15:04:05Z"
 
 func main() {
-	loggers := sLogHarbour.LogInit("app1", "module1", "system1")
+	loggers := logHarbour.LogInit("app1", "module1", "system1")
 
-	sLogHarbour.LogWrite(loggers.ActivityLogger, sLogHarbour.LevelInfo, "spanid1", "correlationid1", time.Now().Format(timeFormat), "bhavya", "127.0.0.1", "newLog", "NA", 1, "This is an activity logger info message", "somekey", "somevalue")
-	sLogHarbour.LogWrite(loggers.ActivityLogger, sLogHarbour.LevelError, "spanid2", "correlationid2", time.Now().Format(timeFormat), "bhavya", "127.0.0.1", "newLog", "NA", 1, "This is an activity logger error message")
+	logHarbour.LogWrite(loggers.ActivityLogger, logHarbour.LevelInfo, "spanid1", "correlationid1", time.Now().Format(timeFormat), "bhavya", "127.0.0.1", "newLog", "NA", 1, "This is an activity logger info message", "somekey", "somevalue")
+	logHarbour.LogWrite(loggers.ActivityLogger, logHarbour.LevelError, "spanid2", "correlationid2", time.Now().Format(timeFormat), "bhavya", "127.0.0.1", "newLog", "NA", 1, "This is an activity logger error message")
 
 	fmt.Println("----")
-	sLogHarbour.LogWrite(loggers.DataChangeLogger, sLogHarbour.LevelInfo, "spanid3", "correlationid3", time.Now().Format(timeFormat), "bhavya", "127.0.0.1", "newLog", "NA", 1, "This is an DataChange logger info message", "somekey", "somevalue", sLogHarbour.DataChg{Field: "amt", OldVal: "100", NewVal: "200"})
-	sLogHarbour.LogWrite(loggers.DataChangeLogger, sLogHarbour.LevelError, "spanid4", "correlationid4", time.Now().Format(timeFormat), "bhavya", "127.0.0.1", "newLog", "NA", 1, "This is an DataChange logger error message", sLogHarbour.DataChg{Field: "amt", OldVal: "100", NewVal: "200"})
+	logHarbour.LogWrite(loggers.DataChangeLogger, logHarbour.LevelInfo, "spanid3", "correlationid3", time.Now().Format(timeFormat), "bhavya", "127.0.0.1", "newLog", "NA", 1, "This is an DataChange logger info message", "somekey", "somevalue", logHarbour.DataChg{Field: "amt", OldVal: "100", NewVal: "200"})
+	logHarbour.LogWrite(loggers.DataChangeLogger, logHarbour.LevelError, "spanid4", "correlationid4", time.Now().Format(timeFormat), "bhavya", "127.0.0.1", "newLog", "NA", 1, "This is an DataChange logger error message", logHarbour.DataChg{Field: "amt", OldVal: "100", NewVal: "200"})
 	fmt.Println("----")
-	sLogHarbour.LogWrite(loggers.DebugLogger, sLogHarbour.LevelDebug, "spanid5", "correlationid5", time.Now().Format(timeFormat), "bhavya", "127.0.0.1", "newLog", "NA", 1, "This is an DEBUG logger message 1", sLogHarbour.DataChg{Field: "amt", OldVal: "100", NewVal: "200"}, sLogHarbour.GetDataChg("qty", "1", "2"))
-	sLogHarbour.ChangeGlobalLogLevel(sLogHarbour.LevelDebug2)
-	sLogHarbour.LogWrite(loggers.DebugLogger, sLogHarbour.LevelDebug1, "spanid6", "correlationid6", time.Now().Format(timeFormat), "bhavya", "127.0.0.1", "newLog", "NA", 1, "This is an DEBUG logger message 2", sLogHarbour.DataChg{Field: "amt", OldVal: "100", NewVal: "200"}, sLogHarbour.GetDataChg("qty", "1", "2"))
-	sLogHarbour.LogWrite(loggers.DebugLogger, sLogHarbour.LevelDebug2, "spanid7", "correlationid7", time.Now().Format(timeFormat), "bhavya", "127.0.0.1", "newLog", "NA", 1, "This is an DEBUG logger message 3")
+	logHarbour.LogWrite(loggers.DebugLogger, logHarbour.LevelDebug, "spanid5", "correlationid5", time.Now().Format(timeFormat), "bhavya", "127.0.0.1", "newLog", "NA", 1, "This is an DEBUG logger message 1", logHarbour.DataChg{Field: "amt", OldVal: "100", NewVal: "200"}, logHarbour.GetDataChg("qty", "1", "2"))
+	logHarbour.ChangeGlobalLogLevel(logHarbour.LevelDebug2)
+	logHarbour.LogWrite(loggers.DebugLogger, logHarbour.LevelDebug1, "spanid6", "correlationid6", time.Now().Format(timeFormat), "bhavya", "127.0.0.1", "newLog", "NA", 1, "This is an DEBUG logger message 2", logHarbour.DataChg{Field: "amt", OldVal: "100", NewVal: "200"}, logHarbour.GetDataChg("qty", "1", "2"))
+	logHarbour.LogWrite(loggers.DebugLogger, logHarbour.LevelDebug2, "spanid7", "correlationid7", time.Now().Format(timeFormat), "bhavya", "127.0.0.1", "newLog", "NA", 1, "This is an DEBUG logger message 3")
 	fmt.Println("---------------------------")
 	fmt.Println("---------------------------")
+	firstFunc(loggers)
 	//main2()
 }
 
-func main2() {
-	logHarbour.LogInit("sampleApp", "moduleName", "systemName")
-	logHarbour.LogWrite(logHarbour.Inf, "spanid11", "correlationid11", time.Now().Format(timeFormat), "bhavya", "127.0.0.1", "newLog", "NA", 1, "This is an activity logger info message")
-	fmt.Println()
-	firstFunc()
-}
-
-func firstFunc() {
+func firstFunc(loggers logHarbour.LogHandles) {
 	//logHarbour.GlobalLogLevel = logHarbour.Dbg
-	logHarbour.LogWrite(logHarbour.Inf, "spanid12", "correlationid12", time.Now().Format(timeFormat), "bhavya", "127.0.0.1", "newLog", "NA", 1, "This is an activity logger info firstFunc", logHarbour.GetDataChg("qty", "100", "200"), logHarbour.GetDataChg("qty", "100", "200"))
+	logHarbour.LogWrite(loggers.ActivityLogger, logHarbour.LevelInfo, "spanid12", "correlationid12", time.Now().Format(timeFormat), "bhavya", "127.0.0.1", "newLog", "NA", 1, "This is an activity logger info firstFunc", logHarbour.GetDataChg("qty", "100", "200"), logHarbour.GetDataChg("qty", "100", "200"))
 	fmt.Println()
-	logHarbour.LogWrite(logHarbour.Err, "spanid13", "correlationid13", time.Now().Format(timeFormat), "bhavya", "127.0.0.1", "newLog", "NA", 1, "This is an activity logger error firstFunc", logHarbour.GetKV("reqIdCustom", "123123123"), logHarbour.GetKV("otherField", "otherfieldvalue"), logHarbour.GetDataChg("qty", "1", "2"), logHarbour.GetDataChg("amt", "100", "200"))
+	logHarbour.LogWrite(loggers.DataChangeLogger, logHarbour.LevelError, "spanid13", "correlationid13", time.Now().Format(timeFormat), "bhavya", "127.0.0.1", "newLog", "NA", 1, "This is an activity logger error firstFunc", "reqIdCustom", "123123123", "otherField", "otherfieldvalue", logHarbour.GetDataChg("qty", "1", "2"), logHarbour.GetDataChg("amt", "100", "200"))
 	fmt.Println()
-	logHarbour.LogWrite(logHarbour.Dbg, "spanid14", "correlationid14", time.Now().Format(timeFormat), "bhavya", "127.0.0.1", "newLog", "NA", 1, "This is an activity logger Debug firstFunc", logHarbour.GetDataChg("qty", "100", "200"))
+	logHarbour.LogWrite(loggers.DebugLogger, logHarbour.LevelDebug1, "spanid14", "correlationid14", time.Now().Format(timeFormat), "bhavya", "127.0.0.1", "newLog", "NA", 1, "This is an activity logger Debug firstFunc", logHarbour.GetDataChg("qty", "100", "200"))
 	fmt.Println()
-	secondFunc()
+	//secondFunc()
 }
 
+/*
 func secondFunc() {
 	logHarbour.LogWrite(logHarbour.Inf, "spanid15", "correlationid15", time.Now().Format(timeFormat), "bhavya", "127.0.0.1", "newLog", "NA", 1, "This is an activity logger info Second Func")
 	logHarbour.LogWrite(logHarbour.Err, "spanid16", "correlationid16", time.Now().Format(timeFormat), "bhavya", "127.0.0.1", "newLog", "NA", 1, "This is an activity logger error Second Func")
@@ -62,3 +56,4 @@ func thirdFunc() {
 	logHarbour.LogWrite(logHarbour.Err, "spanid20", "correlationid20", time.Now().Format(timeFormat), "bhavya", "127.0.0.1", "newLog", "NA", 1, "This is an activity logger error Third Func")
 	logHarbour.LogWrite(logHarbour.Dbg, "spanid21", "correlationid21", time.Now().Format(timeFormat), "bhavya", "127.0.0.1", "newLog", "NA", 1, "This is an activity logger debug Third Func")
 }
+*/
